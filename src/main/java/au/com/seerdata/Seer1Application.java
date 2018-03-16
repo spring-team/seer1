98adb75d738321ee34f8653a1cb508f3fc0a4d9d
Rod Johnson <johnsonroda@gmail.com>
2018-03-16 17:06:38 +1100

    Initial commit from Atomist

---
+++ src/main/java/au/com/seerdata/Seer1Application.java
@@ -0,0 +1,12 @@
+package au.com.seerdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seer1Application.class, args);
+	}
+}
